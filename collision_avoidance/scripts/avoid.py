3f89229986b5f881b98623dcff1a28149e2b6147
Liam Wellacott <liamwellacott@yahoo.co.uk>
2019-09-30 19:00:52 +0100

    Added some left/right primitives, doesn't work yet...

--- collision_avoidance/scripts/avoid.py
+++ collision_avoidance/scripts/avoid.py
@@ -6,31 +6,89 @@ from geometry_msgs.msg import Twist
 
 lin = 1
 turn = 0
+turning = False
 
-def scanCallback(msg):
+def left():
     global lin
     global turn
 
-    if msg.ranges[0] > 0.12 and msg.ranges[0] < 0.5:
-    rospy.loginfo(msg.ranges[0])
-        turn = 1
-        lin = 0
-    else:
-        turn = 0
-        lin = 1
+    rospy.loginfo("turning left")
+    turn = 1
+    lin = 0
+
+def right():
+    global lin
+    global turn
+
+    rospy.loginfo("turning right")
+    turn = -1
+    lin = 0
+
+def forward():
+    global lin
+    global turn
+
+    rospy.loginfo("going forward")
+    turn = 0
+    lin = 1
+
+def scanCallback(msg):
+   
+    global turning
+
+    front_laser = msg.ranges[0]
+    left_laser = msg.ranges[89]
+    right_laser = msg.ranges[269]
+
+    rospy.loginfo("new data")
+
+    # if we detect an object and not turning
+    if (not turning) and front_laser > 0.12 and front_laser < 0.4: 
+        
+        rospy.loginfo("starting to turn")
+
+        #start turning
+        turning = True
+
+        # if left is closer to you (smaller sensor value)
+        # go this direction until free
+        if left_laser > right_laser:
+            left()
+        else:
+            right()
+    
+    # if we are already turning (higher threshold than initial turning)
+    if turning and front_laser > 0.6:
+
+        rospy.loginfo("finishing turn")
+
+        # we exceeded threshold of space in front, go forward
+        turning = False
+        forward()
 
 def move_motor(pub):
     global lin
     global turn
 
+    # create message object 
     mc = Twist()
+
+    # set speed
     mc.linear.x = lin
     mc.angular.z = turn
+
+    # publish message
     pub.publish(mc)
 
-def moveloop(pub):  
+def moveloop(pub): 
+    
+    # set message rate 
     rate = rospy.Rate(20)
 
+    # set initial direction
+    forward()
+
+    # loop updating 
     while not rospy.is_shutdown():
         move_motor(pub)
         rate.sleep()        
