241f482874e965cf1c65659fbfa5a67956e49a40
Pierre Nicolay <p.nicolay@student.uliege.be>
2019-10-14 18:03:23 +0200

    milestone 2 package creation

---
+++ milestone2/scripts/particle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import rospy
+
+class robot:
+    def __init__(self):
+
+    def setPose(self, pose):
+
+    def setNoise(self, noise):
+
+    def sense(self):
+
+    def measureProb(self):
+
+    def move(self):
+
+    
