fce84560f094d02b8d63ad95b640473f26a69b39
Pierre Nicolay <nicolaypierre.95@gmail.com>
2019-11-27 12:46:45 +0000

    results and particle_test to generate the plots

--- milestone2/scripts/particle_test.py
+++ milestone2/scripts/particle_test.py
@@ -5,9 +5,10 @@ from utile import Map
 import json
 import sys
 import numpy as np
+import scipy.signal as sig
 
-MAP_FILE = "maps/rss_offset_box1.json"
-MAX_LOG = 500
+MAP_FILE = "maps/rss_offset.json"
+MAX_LOG = 1200
 map = Map(MAP_FILE)
 
 
@@ -20,7 +21,20 @@ def parseFileParticle(file):
         t = []
         s = []
         m = []
-        for i in range(MAX_LOG):
+        path = []
+        path.append(data["0path"])
+        path.append(data["1path"])
+        path.append(data["2path"])
+        path.append(data["3path"])
+        path.append(data["4path"])
+
+        path_np = np.array(data["0path"])
+        path_np = np.vstack((path_np, np.array(data["1path"])))
+        path_np = np.vstack((path_np, np.array(data["2path"])))
+        path_np = np.vstack((path_np, np.array(data["3path"])))
+        path_np = np.vstack((path_np, np.array(data["4path"])))
+
+        for i in np.arange(1, MAX_LOG):
             r.append(data["{}rPose".format(i)])
             t.append(data["{}tPose".format(i)])
             s.append(data["{}tScan".format(i)])
@@ -43,10 +57,13 @@ def parseFileParticle(file):
             r_ = data["{}rPose".format(i)]
             t_ = data["{}tPose".format(i)]
 
-            if i % 10 == 0:
+
+            if i % 20 == 0:
+
                 ax.clear()
                 fig, ax = map.plotMap(fig, ax)
-
+                r__ = np.array(r)
+                ax.plot(r__[:, 0], r__[:, 1], 'k', label="True position", linewidth=2)
                 # Plotting robot position
                 ax.scatter(r_[0], r_[1], c='r', s=200)
 
@@ -58,11 +75,17 @@ def parseFileParticle(file):
                 ax.quiver(t_[0], t_[1], .00001, .00001, angles=[np.rad2deg(t_[2])], scale=1./10000., scale_units="xy",
                        units="xy", color="k", pivot="mid",width=0.01, headwidth=0.1, headlength=0.1)
 
-
+                '''
                 for p in particles:
                     ax.scatter(p[0], p[1], c='b')
                     ax.quiver(p[0], p[1], .000005, .000005, angles=[np.rad2deg(p[2])], scale=1./10000., scale_units="xy",
                            units="xy", color="y", pivot="mid",width=0.001, headwidth=0.001, headlength=0.00005)
+                '''
+                #for p in path:
+                    #p = np.array(p)
+                ax.plot(path_np[:, 0], path_np[:, 1], c='g', label="trajectory", linewidth=2)
+                ax.legend()
+                plt.title("True position of the robot w.r.t the planned trajectory")
                 plt.draw()
                 plt.pause(0.01)
 
@@ -70,21 +93,54 @@ def parseFileParticle(file):
     t = np.array(t)
     m = np.array(m).reshape(-1, 8)
     s = np.array(s)
+    print(path_np.shape)
+    #print(p.shape)
+
+    fs = 5.
+    cutoff = 0.2
+    order = 5.
+
+    nyquist = fs / 2.
+
+    b, a = sig.butter(order, cutoff / nyquist)
+    if not np.all(np.abs(np.roots(a)) < 1):
+        raise PsolaError('Filter with cutoff at {} Hz is unstable given '
+                         'sample frequency {} Hz'.format(cutoff, fs))
+    r_x = sig.filtfilt(b, a, r[:, 0], method='gust')
+    r_y = sig.filtfilt(b, a, r[:, 1], method='gust')
+    r_yaw = sig.filtfilt(b, a, r[:, 2], method='gust')
 
     diff = m - s
 
     fig, ax = plt.subplots(2, 2)
-    x = range(MAX_LOG)
-    ax[0, 0].plot(x, r[:,0], 'r')
-    ax[0, 0].plot(x, t[:,0], 'b')
+    x = range(1, MAX_LOG)
+    ax[0, 0].plot(x, r[:, 0], 'r')
+    ax[0, 0].plot(x, r_x, 'k')
+    ax[0, 0].plot(x, t[:, 0], 'b')
+    #ax[0, 0].plot(x, path_np[:, 0], 'g')
+    ax[0, 0].set_xlabel("Step")
+    ax[0, 0].set_ylabel("Position (m)")
+    ax[0, 0].set_title("X position")
 
-    ax[0, 1].plot(x, r[:,1], 'r')
-    ax[0, 1].plot(x, t[:,1], 'b')
+    ax[0, 1].plot(x, r[:, 1], 'r')
+    ax[0, 1].plot(x, r_y, 'k')
+    ax[0, 1].plot(x, t[:, 1], 'b')
+    #ax[0, 0].plot(x, path_np[:, 1], 'g')
+    ax[0, 1].set_xlabel("Step")
+    ax[0, 1].set_ylabel("Position (m)")
+    ax[0, 1].set_title("Y position")
 
     ax[1, 0].plot(x, r[:,2], 'r')
+    ax[1, 0].plot(x, r_yaw, 'k')
     ax[1, 0].plot(x, t[:,2], 'b')
+    ax[1, 0].set_xlabel("Step")
+    ax[1, 0].set_ylabel("Position (m)")
+    ax[1, 0].set_title("Yaw position")
+
+    fig.suptitle("Pose evolution")
 
     fig2, ax2 = plt.subplots(4, 2)
+    '''
     ax2[0, 0].plot(x, diff[:, 0], 'g')
     ax2[0, 1].plot(x, diff[:, 1], 'g')
     ax2[1, 0].plot(x, diff[:, 2], 'g')
@@ -93,32 +149,49 @@ def parseFileParticle(file):
     ax2[2, 1].plot(x, diff[:, 5], 'g')
     ax2[3, 0].plot(x, diff[:, 6], 'g')
     ax2[3, 1].plot(x, diff[:, 7], 'g')
-
     '''
+
     ax2[0, 0].plot(x, m[:, 0], 'r')
     ax2[0, 0].plot(x, s[:, 0], 'b')
+    ax2[0, 0].set_xlabel("Step")
+    ax2[0, 0].set_ylabel("Reading (m)")
 
     ax2[0, 1].plot(x, m[:, 1], 'r')
     ax2[0, 1].plot(x, s[:, 1], 'b')
+    ax2[0, 1].set_xlabel("Step")
+    ax2[0, 1].set_ylabel("Reading (m)")
 
     ax2[1, 0].plot(x, m[:, 2], 'r')
     ax2[1, 0].plot(x, s[:, 2], 'b')
+    ax2[1, 0].set_xlabel("Step")
+    ax2[1, 0].set_ylabel("Reading (m)")
 
     ax2[1, 1].plot(x, m[:, 3], 'r')
     ax2[1, 1].plot(x, s[:, 3], 'b')
+    ax2[1, 1].set_xlabel("Step")
+    ax2[1, 1].set_ylabel("Reading (m)")
 
     ax2[2, 0].plot(x, m[:, 4], 'r')
     ax2[2, 0].plot(x, s[:, 4], 'b')
+    ax2[2, 0].set_xlabel("Step")
+    ax2[2, 0].set_ylabel("Reading (m)")
 
     ax2[2, 1].plot(x, m[:, 5], 'r')
     ax2[2, 1].plot(x, s[:, 5], 'b')
+    ax2[2, 1].set_xlabel("Step")
+    ax2[2, 1].set_ylabel("Reading (m)")
 
     ax2[3, 0].plot(x, m[:, 6], 'r')
     ax2[3, 0].plot(x, s[:, 6], 'b')
+    ax2[3, 0].set_xlabel("Step")
+    ax2[3, 0].set_ylabel("Reading (m)")
 
     ax2[3, 1].plot(x, m[:, 7], 'r')
     ax2[3, 1].plot(x, s[:, 7], 'b')
-    '''
+    ax2[3, 1].set_xlabel("Step")
+    ax2[3, 1].set_ylabel("Reading (m)")
+
+    fig2.suptitle("Measurements")
     plt.show()
 
 def parseFileOdom(file):
